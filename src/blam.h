2c0352b40c98114cfed712105fefb59839ccaeb2
Brett Nash <nash@nash.id.au>
2012-12-10 06:28:14 +1100

    A todo for blam

--- src/blam.h
+++ src/blam.h
@@ -1,6 +1,8 @@
 
 
 struct blam {
+	// FIXME: Should also detect merging, so two writes next to each other
+	// get merged.
 	int (*write)(struct blam *, const void *data, int len);
 	int (*flush)(struct blam *);
 	int (*close)(struct blam *);
