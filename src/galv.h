c8dfa94b41dc126a0daaa1bf1263a4c88eb74630
Brett Nash <nash@nash.id.au>
2013-11-05 16:11:07 +0800

    Add typesafe max/min macros

--- src/galv.h
+++ src/galv.h
@@ -4,6 +4,17 @@
 #include "debug.h"
 #include "blam.h"
 
+#define MAX(a,b) ({	\
+		__typeof__ (a) _a = (a); \
+		__typeof__ (b) _b = (b); \
+		_a > _b ? _a : _b;	\
+		})
+#define MIN(a,b) ({	\
+		__typeof__ (a) _a = (a); \
+		__typeof__ (b) _b = (b); \
+		_a < _b ? _a : _b;	\
+		})
+
 extern int DEBUG_LEVEL;
 
 extern lua_State *L;
